280a1d967440c07b0069d269c9a6642974f11b03
Cuhwaii <59039810+shafay39@users.noreply.github.com>
2023-01-31 13:09:08 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+let P1 = 0
+let ROUNDS = 0
+let TIES = 0
+let P2 = 0
+input.onButtonPressed(Button.A, function () {
+    P1 += 1
+    ROUNDS += 1
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        `)
+    update_scores()
+})
+input.onButtonPressed(Button.AB, function () {
+    TIES += 1
+    ROUNDS += 1
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    update_scores()
+})
+input.onButtonPressed(Button.B, function () {
+    P2 += 1
+    ROUNDS += 1
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+    update_scores()
+})
 input.onGesture(Gesture.Shake, function () {
     OLED.init(128, 64)
     OLED.writeStringNewLine("wow guys this is so pinteresque")
@@ -6,28 +46,26 @@ input.onGesture(Gesture.Shake, function () {
     TIES = 0
     ROUNDS = 0
     basic.pause(2000)
-    OLED.clear()
+    reset()
 })
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("wow guys this is so pinteresque")
+    P1 = 0
+    P2 = 0
+    TIES = 0
+    ROUNDS = 0
+    basic.pause(2000)
+    OLED.clear()
+    update_scores()
+}
 function update_scores () {
     OLED.clear()
-    OLED.writeStringNewLine("PLAYER: 1" + P1)
+    OLED.writeStringNewLine("PLAYER 1: " + P1)
     OLED.newLine()
-    OLED.writeStringNewLine("PLAYER: 2" + P2)
+    OLED.writeStringNewLine("PLAYER 2: " + P2)
     OLED.newLine()
-    OLED.writeStringNewLine("TIES" + TIES)
+    OLED.writeStringNewLine("TIES " + TIES)
     OLED.newLine()
-    OLED.writeStringNewLine("ROUNDS" + ROUNDS)
+    OLED.writeStringNewLine("ROUNDS " + ROUNDS)
 }
-let ROUNDS = 0
-let TIES = 0
-let P2 = 0
-let P1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("wow guys this is so pinteresque")
-P1 = 0
-P2 = 0
-TIES = 0
-ROUNDS = 0
-basic.pause(2000)
-OLED.clear()
-update_scores()
